a6e206e4d8a1e84ddd2c73ecfd9464e35d90ca3f
Daniel White <DanielWhite94@server.fake>
2015-06-23 13:10:05 +0100

    Added ViewPoint class.

---
+++ src/viewpoint.cpp
@@ -0,0 +1,33 @@
+#include "viewpoint.h"
+
+namespace RayCast {
+
+	ViewPoint::ViewPoint(float X, float Y, float Angle, float Fov, float MaxDist) {
+		x=X;
+		y=Y;
+		angle=Angle;
+		fov=Fov;
+		maxDist=MaxDist;
+	}
+
+	float ViewPoint::getX(void) const {
+		return x;
+	}
+
+	float ViewPoint::getY(void) const {
+		return y;
+	}
+
+	float ViewPoint::getAngle(void) const {
+		return angle;
+	}
+
+	float ViewPoint::getFov(void) const {
+		return fov;
+	}
+
+	float ViewPoint::getMaxDist(void) const {
+		return maxDist;
+	}
+
+};
---
+++ src/viewpoint.h
@@ -0,0 +1,26 @@
+#ifndef RAYCAST_VIEWPOINT_H
+#define RAYCAST_VIEWPOINT_H
+
+#include <cmath>
+
+namespace RayCast {
+
+	class ViewPoint {
+	public:
+		ViewPoint(float X, float Y, float Angle, float Fov=M_PI/3.0, float MaxDist=64.0);
+
+		float getX(void) const ;
+		float getY(void) const ;
+		float getAngle(void) const ;
+		float getFov(void) const ;
+		float getMaxDist(void) const ;
+	private:
+		float x, y;
+		float angle;
+		float fov; // Field-of-view, radians.
+		float maxDist; // Maximum viewing distance.
+	};
+
+};
+
+#endif
