62ece16aa188ae556e495d89c99c27ea89aa34c3
Daniel White <DanielWhite94@server.fake>
2015-06-28 21:06:06 +0100

    Implemented top-down view in renderer.

--- src/Games/raycast/renderer.cpp
+++ src/Games/raycast/renderer.cpp
@@ -35,6 +35,9 @@ namespace RayCast {
 
 #ifdef EXTRA
 	void Renderer::renderTopDown(const ViewPoint &vPoint) {
+		#define SX(X) ((screenW/2+cellW*(vPoint.getX()-(X))).getInt())
+		#define SY(Y) ((screenH/2+cellH*(vPoint.getY()-(Y))).getInt())
+
 		const int cellW=16;
 		const int cellH=16;
 
@@ -43,6 +46,48 @@ namespace RayCast {
 
 		// Clear screen.
 		EsploraTFT.background(0, 0, 0);
+
+		// Debugging grid.
+		int cellsWide=screenW/cellW+2;
+		int cellsHigh=screenH/cellH+2;
+		int minMapX=vPoint.getX().floor()-cellsWide/2;
+		int minMapY=vPoint.getY().floor()-cellsHigh/2;
+		int maxMapX=minMapX+cellsWide;
+		int maxMapY=minMapY+cellsHigh;
+		int mapX, mapY;
+		EsploraTFT.stroke(196, 196, 196);
+		int x, y;
+		for(x=minMapX;x<=maxMapX;++x)
+			EsploraTFT.line(SX(x), 0, SX(x), screenH);
+		for(y=minMapY;y<=maxMapY;++y)
+			EsploraTFT.line(0, SY(y), screenW, SY(y));
+
+		// Trace ray.
+		Ray ray(vPoint.getX(), vPoint.getY(), vPoint.getAngle());
+		int i;
+		for(i=0;i<10;++i) {
+			// Draw highlighted cell.
+			EsploraTFT.stroke(255, 0, 0);
+			EsploraTFT.fill(255, 0, 0);
+			EsploraTFT.rect(SX(ray.getX())+1, SY(ray.getY())+1, cellW-2, cellH-2);
+
+			// Advance ray.
+			ray.next();
+		}
+
+		// Viewpoint cross-hair.
+		int k=2;
+		EsploraTFT.stroke(0, 0, 255);
+		EsploraTFT.line(SX(vPoint.getX())-k, SY(vPoint.getY())-k, SX(vPoint.getX())+k, SY(vPoint.getY())+k);
+		EsploraTFT.line(SX(vPoint.getX())-k, SY(vPoint.getY())+k, SX(vPoint.getX())+k, SY(vPoint.getY())-k);
+
+		// Line of sight.
+		EsploraTFT.stroke(0, 255, 0);
+		Fixed len=Fixed(16.0);
+		EsploraTFT.line(SX(vPoint.getX()), SY(vPoint.getY()), SX(vPoint.getX()+Fixed::cos(vPoint.getAngle())*len), SY(vPoint.getY()+Fixed::sin(vPoint.getAngle())*len));
+
+		#undef SX
+		#undef SY
 	}
 #endif
 
