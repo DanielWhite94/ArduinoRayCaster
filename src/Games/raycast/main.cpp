d36af720230e48392b5db4c14cfedefc86ab5d97
Daniel White <DanielWhite94@server.fake>
2015-06-28 21:05:46 +0100

    Added movement logic to main.cpp.

--- src/Games/raycast/main.cpp
+++ src/Games/raycast/main.cpp
@@ -14,8 +14,23 @@ int main(int argc, char **argv) {
 	EsploraTFT.begin();
 	renderer=new Renderer();
 
+	const Fixed moveSpeed=0.2;
+	const Fixed turnSpeed=Fixed(M_PI/18.0);
+
+	ViewPoint vPoint(Fixed(13.4), Fixed(8.0), Fixed(M_PI));
+
 	while(1) {
-		ViewPoint vPoint(0, 0, 0);
+		// Check events.
+		if (Esplora.readButton(SWITCH_LEFT)==HIGH)
+			vPoint.turn(-turnSpeed);
+		if (Esplora.readButton(SWITCH_RIGHT)==HIGH)
+			vPoint.turn(turnSpeed);
+		if (Esplora.readButton(SWITCH_UP)==HIGH)
+			vPoint.move(moveSpeed);
+		if (Esplora.readButton(SWITCH_DOWN)==HIGH)
+			vPoint.move(-moveSpeed);
+
+		// Redraw.
 		renderer->render(vPoint);
 #if SDL_MAJOR_VERSION == 2
 		EsploraTFT.tick();
